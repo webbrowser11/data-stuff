750732c44e3c2b02e73fa904af24c5e8865c5684
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-18 16:24:43 -0700

    add data-1.py

---
+++ src/data-1.py
@@ -0,0 +1,7 @@
+# list data and varible data
+example = [1,2,3]
+print(example)
+print("this prints data from a list you can do a lot with this.")
+exmaplevar = 10
+print(exmaplevar)
+print("this prints data from a varible this is very useful too.")
